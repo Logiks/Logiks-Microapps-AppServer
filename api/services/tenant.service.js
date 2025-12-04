98a0ba07ac91d4a9123c4d711187ab570d88fd57
Bismay M <bismay@smartinfologks.com>
2025-12-04 19:30:11 +0530

    Theme CSS

--- api/services/tenant.service.js
+++ api/services/tenant.service.js
@@ -48,6 +48,33 @@ module.exports = {
 					);
 				}
 			}
+		},
+
+		theme: {
+			rest: {
+				method: "GET",
+				fullPath: "/api/theme/:themeid?"
+			},
+			async handler(ctx) {
+
+				if(!ctx.params.themeid) ctx.params.themeid = "default";
+
+				//const appLayoutFile = `misc/apps/${ctx.meta.appInfo.appid}/layouts/${ctx.params.layoutid}.json`;
+				const themeFile = `misc/themes/${ctx.params.themeid}/style.css`;
+				if(fs.existsSync(themeFile)) {
+					const themeData = fs.readFileSync(themeFile, "utf8");
+					
+					ctx.meta.$responseType = "text/css";
+					return themeData;
+				} else {
+					throw new Errors.MoleculerClientError(
+						"Invalid Application Layout Identifier",
+						401,
+						"INVALID_LAYOUT_KEY",
+						ctx.params.themeid
+					);
+				}
+			}
 		}
 	}
 };
