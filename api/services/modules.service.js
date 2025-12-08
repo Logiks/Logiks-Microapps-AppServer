de87b72e31ecf5491dd4ee56d9fcf401fb99798a
Bismay M <bismay@smartinfologks.com>
2025-12-08 11:15:21 +0530

    Modules and Components

--- api/services/modules.service.js
+++ api/services/modules.service.js
@@ -1,10 +1,21 @@
 "use strict";
 
+// /api/modules/reports/doc.main
+// /api/modules/forms/doc.main
+// /api/modules/dashboards/doc.main
+
+// PAGES
+// /api/modules/docs/main
+// /api/modules/pages/docs.main
+
+// Components
+// /api/modules/components/docs.main
+
 module.exports = {
 	name: "modules",
 
 	actions: {
-		listModules: {
+		fetchModule: {
 			rest: {
 				method: "GET",
 				fullPath: "/api/modules/:module/:item?"///:action/:item?
@@ -12,15 +23,41 @@ module.exports = {
 			async handler(ctx) {
 				const moduleName = ctx.params.module;
 				var item = ctx.params.item.split(".");
-				var pluginID = item[0];
-				var submoduleFile = item[1];
-				console.log("MODULE_HANDLER", ctx.params);
 				
-				const fileContent = await ctx.call(`${pluginID}.source`, {folder: moduleName, file: submoduleFile});
-                return {
-					"component": moduleName,
-					"content": fileContent
-				};
+				console.log("MODULE_HANDLER", ctx.params, item.length);
+
+				if(item.length>1) {
+					var pluginID = item[0];
+					var submoduleFile = item[1];
+					var modname = moduleName.substring(moduleName.length-1,moduleName.length)=="s"?moduleName.substring(0,moduleName.length-1):moduleName;
+
+					const fileContent = await ctx.call(`${pluginID}.source`, {folder: moduleName, file: submoduleFile});
+					return {
+						"component": modname,
+						"content": fileContent
+					};
+				} else {
+					const fileContent = await ctx.call(`${pluginID}.source`, {folder: "pages", file: submoduleFile});
+					return {
+						"component": "page",
+						"content": fileContent
+					};
+				}
+			}
+		},
+		fetchModule: {
+			rest: {
+				method: "GET",
+				fullPath: "/api/modules/:module/component/:item?"
+			},
+			async handler(ctx) {
+				const moduleName = ctx.params.module;
+				var item = ctx.params.item.split(".");
+				
+				console.log("MODULE_COMPONENT_HANDLER", ctx.params, item.length);
+
+				const fileContent = await ctx.call(`${pluginID}.source`, {folder: "components", file: submoduleFile});
+				return fileContent;
 			}
 		},
 		service: {
