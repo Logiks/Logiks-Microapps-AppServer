70383b1d85a95f2a2c6504916b0fa99fd2551436
Bismay M <bismay@smartinfologks.com>
2026-01-11 21:24:23 +0530

    My Password Update

--- api/services/application.service.js
+++ api/services/application.service.js
@@ -377,6 +377,22 @@ module.exports = {
 					"info": await ctx.call("auth.getMyInfo")
 				}
 			}
+		},
+
+		passwordUpdate: {
+			rest: {
+				method: "POST",
+				fullPath: "/api/me/updatepass"
+			},
+			params:{
+				oldPassword: "string",
+				newPassword: "string",
+				passwordType: { type: "string", optional: true }
+			},
+			async handler(ctx) {
+				var response = await USERS.updateUserPassword(ctx.meta.user.guid, ctx.meta.user.userId, ctx.params.newPassword, ctx.params.oldPassword);
+				return response;
+			}
 		}
     },
     methods: {
