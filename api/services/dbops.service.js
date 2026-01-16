db6d8fcd06707e83365c65d68f7a5918bd6d882f
Bismay M <bismay@smartinfologks.com>
2026-01-16 20:03:49 +0530

    checking

--- api/services/dbops.service.js
+++ api/services/dbops.service.js
@@ -90,7 +90,7 @@ module.exports = {
                 // console.log(dataFields, MISC.generateDefaultDBRecord(ctx, false));
                 //Single Insert
 
-                forcefill = QUERY.updateWhereFromEnv(forcefill, await QUERY.processMetaInfo(ctx.meta));
+                forcefill = await QUERY.updateWhereFromEnv(forcefill, await QUERY.processMetaInfo(ctx.meta));
                 if(forcefill && Object.keys(forcefill)>0) dataFields = _.extend(dataFields, forcefill);
 console.log("FORCEFILL", forcefill, dataFields, (forcefill && Object.keys(forcefill)>0));
                 const dbResponse = await _DB.db_insertQ1("appdb", sqlTable, dataFields);
