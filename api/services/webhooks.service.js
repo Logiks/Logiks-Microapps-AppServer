6d49cf729e80c12729831fbf7c42c717e659c709
Bismay M <bismay@smartinfologks.com>
2026-02-02 17:03:05 +0530

    Webhook renaming

--- api/services/webhooks.service.js
+++ api/services/webhooks.service.js
@@ -18,6 +18,21 @@ module.exports = {
                 const response = await WEBHOOKS.receiveRequest(ctx.params.webhookid, ctx);
                 return response;
             }
+        },
+
+        runWebhook1: {
+            rest: {
+				method: "GET",
+				fullPath: "/webhooks/:webhookid"
+                // path: "/:webhookid"
+			},
+            params: {
+                // cmd: "string"
+            },
+			async handler(ctx) {
+                const response = await WEBHOOKS.receiveRequest(ctx.params.webhookid, ctx);
+                return response;
+            }
         }
     }
 }
