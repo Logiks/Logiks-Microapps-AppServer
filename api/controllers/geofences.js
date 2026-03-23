e412cb65cc71e24a2ef16c306d2e2af49e436d8b
Bismay M <bismay@smartinfologks.com>
2026-03-23 12:09:58 +0530

    GeoFence Controller > Allow open table

--- api/controllers/geofences.js
+++ api/controllers/geofences.js
@@ -9,7 +9,7 @@ module.exports = {
         return true; //Public Controller
     },
 
-    findGeofence: async function(guid, geolocation, groupid='general', fenceType = "polygon", limit = 10) {
+    findGeofence: async function(guid, geolocation, groupid='general', fenceType = "polygon", limit = 10, geoTable = "lgks_geofences") {
         var geoArr = geolocation.split(",");
         const lat = geoArr[0];
         const lng = geoArr[1];
@@ -17,12 +17,12 @@ module.exports = {
         
         switch(fenceType) {
             case "polygon":
-                var SQL_QUERY = `SELECT *, ST_Distance_Sphere(ST_SRID(POINT(${lng}, ${lat}), 4326), geofence_center) as distance FROM lgks_geofences WHERE ST_Contains(geofence_area, ST_SRID(POINT(${lng}, ${lat}), 4326)) AND blocked='false' AND (guid='global' OR guid='${guid}') AND geofence_groupid='${groupid}' LIMIT ${limit}`;
+                var SQL_QUERY = `SELECT *, ST_Distance_Sphere(ST_SRID(POINT(${lng}, ${lat}), 4326), geofence_center) as distance FROM ${geoTable} WHERE ST_Contains(geofence_area, ST_SRID(POINT(${lng}, ${lat}), 4326)) AND blocked='false' AND (guid='global' OR guid='${guid}') AND geofence_groupid='${groupid}' LIMIT ${limit}`;
                 var dbData = await _DB.db_query("appdb", SQL_QUERY, {});
                 return dbData?.results;
                 break;
             case "circular":
-                var SQL_QUERY = `SELECT *, ST_Distance_Sphere(ST_SRID(POINT(${lng}, ${lat}), 4326), geofence_center) as distance FROM lgks_geofences WHERE ST_Distance_Sphere(ST_SRID(POINT(${lng}, ${lat}), 4326), geofence_center) <= ${radius_in_meters} AND blocked='false' AND (guid='global' OR guid='${guid}') AND geofence_groupid='${groupid}' LIMIT ${limit}`;
+                var SQL_QUERY = `SELECT *, ST_Distance_Sphere(ST_SRID(POINT(${lng}, ${lat}), 4326), geofence_center) as distance FROM ${geoTable} WHERE ST_Distance_Sphere(ST_SRID(POINT(${lng}, ${lat}), 4326), geofence_center) <= ${radius_in_meters} AND blocked='false' AND (guid='global' OR guid='${guid}') AND geofence_groupid='${groupid}' LIMIT ${limit}`;
                 var dbData = await _DB.db_query("appdb", SQL_QUERY, {});
                 return dbData?.results;
                 break;
@@ -31,12 +31,12 @@ module.exports = {
         }
     },
     
-    listGeofences: async function(guid, geolocation, groupid='general', limit = 10) {
+    listGeofences: async function(guid, geolocation, groupid='general', limit = 10, geoTable = "lgks_geofences") {
         var geoArr = geolocation.split(",");
         const lat = geoArr[0];
         const lng = geoArr[1];
 
-        var SQL_QUERY = `SELECT *, ST_Distance_Sphere(ST_SRID(POINT(${lng}, ${lat}), 4326), geofence_center) as distance FROM lgks_geofences WHERE blocked='false' AND (guid='global' OR guid='${guid}') AND geofence_groupid='${groupid}' ORDER BY ST_Distance_Sphere(ST_SRID(POINT(${lng}, ${lat}), 4326), geofence_center) ASC LIMIT ${limit}`;
+        var SQL_QUERY = `SELECT *, ST_Distance_Sphere(ST_SRID(POINT(${lng}, ${lat}), 4326), geofence_center) as distance FROM ${geoTable} WHERE blocked='false' AND (guid='global' OR guid='${guid}') AND geofence_groupid='${groupid}' ORDER BY ST_Distance_Sphere(ST_SRID(POINT(${lng}, ${lat}), 4326), geofence_center) ASC LIMIT ${limit}`;
         var dbData = await _DB.db_query("appdb", SQL_QUERY, {});
         return dbData?.results;
     },
